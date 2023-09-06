b7d850b82b87efcbca31484021bf5c1f5fd1f52a
Gabo Esquivel <contact@gaboesquivel.com>
2023-09-06 07:37:17 -0600

    fix: typo

--- src/pages/home/async-on-event-conditional.tsx
+++ src/pages/home/async-on-event-conditional.tsx
@@ -6,7 +6,7 @@ export function AsyncOnEnventConditial(){
   const [state, callFn] = useAsyncFn(someVanillaConditional)
   
   return <div>
-    <h1>Async on Event in Conditionally</h1>
+    <h1>Async on Event Conditionally</h1>
     <button onClick={callFn}>get data in conditionally</button>
     <pre>{JSON.stringify(state)}</pre></div>
 }
